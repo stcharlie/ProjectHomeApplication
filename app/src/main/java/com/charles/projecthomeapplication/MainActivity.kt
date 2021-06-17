914ad897c63aad200d2abf8e9babb736ce1acdf9
AndriodProjects <charlieakarekor@gmail.com>
2021-06-17 19:30:17 +0100

    First Commit

---
+++ app/src/main/java/com/charles/projecthomeapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.charles.projecthomeapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
